7688aef908aa8f5c21b0615135b2111488b134b7
emilvol-Java <emilvol-Java@172.16.1.68>
2017-03-31 16:54:53 +0200

    wer

--- test/com/virtualpairprogrammers/isbntools/ValidateISBNtest.java
+++ test/com/virtualpairprogrammers/isbntools/ValidateISBNtest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ValidateISBNtest {
 
 	@Test
-	public void checkAVAlidISBN() {
+	public void checkAValid10DigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue("first value", result);
@@ -16,19 +16,41 @@ public class ValidateISBNtest {
 	}
 	
 	@Test
-	public void ISBNNumbersEndingInAnXAreValid(){
+	public void checkAValid13DigitISBN(){
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9781853260087");
+		assertTrue("first value", result);
+		result = validator.checkISBN("9781853267338");
+ 		assertTrue("second value",result);
+
+
+	}
+	
+	
+	@Test
+	public void TenDigitISBNNumbersEndingInAnXAreValid(){
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("012000030X");
 		assertTrue(result);
 
 	}
+			
 	
 	@Test
-	public void checkAnInvalidISBN() {
+	public void checkAnInvalid10DigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449117");
 		assertFalse(result);
 	}
+
+	@Test
+	public void checkAnInvalid13DigitISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9781853267336");
+		assertFalse(result);
+	
+	}
+
 	
 	@Test(expected = NumberFormatException.class)
 	public void nineDigitISBNsAreNotAllowed(){
